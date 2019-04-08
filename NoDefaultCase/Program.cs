a8185647224b136206691738b76e31139ca1d738
Martin Eyles <martin.eyles@lineview.com>
2019-04-08 08:42:05 +0000

    Add project files.

---
+++ NoDefaultCase/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NoDefaultCase
+{
+    public static class Program
+    {
+        public const string Database = "MongoDB";
+
+        public static string GetDb()
+        {
+            switch (Database)
+            {
+                case "MongoDB":
+                    return Database;
+            }
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine($"The value returned is \"{GetDb()}\".");
+            Console.WriteLine("Press a key to exit");
+            Console.ReadKey();
+        }
+    }
+}
